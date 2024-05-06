38b96a05f94f0e15b4b9a6cf0f94d6578134573f
haqueb2 <64923001+haqueb2@users.noreply.github.com>
2024-05-06 09:00:21 -0400

    Updated shell scripts

--- exon_length.sh
+++ exon_length.sh
@@ -32,42 +32,7 @@ while IFS= read -r nm_number || [[ -n "$nm_number" ]]; do
   fi
 
   # Execute the MySQL query and append the results to the output file
-  mysql -h "${host}" -u "${user}" -D "${database}" -e "SELECT bin,name,chrom,strand,txStart,txEnd,cdsStart,cdsEnd,exonCount,score,name2,cdsStartStat,cdsEndStat, GROUP_CONCAT(DISTINCT exonFrames SEPARATOR ';'), GROUP_CONCAT(DISTINCT exonStarts SEPARATOR ';'), GROUP_CONCAT(DISTINCT exonEnds SEPARATOR ';') FROM refGene WHERE name='${nm_number}' GROUP BY name" -N | awk -F'\t' 'BEGIN{OFS=","} {gsub(/,/,";", $14); gsub(/,/,";", $15); gsub(/,/,";", $16); gsub(/; /,";", $15); gsub(/; /,";", $16); gsub(/ /,"", $15); gsub(/ /,"", $16); split($14, exonFramesArr, ";"); split($15, exonStartsArr, ";"); split($16, exonEndsArr, ";"); exonLengths=""; exonStarts=""; exonEnds=""; strand=$4; for (i=1; i<=length(exonFramesArr); i++) { lengthDiff = exonEndsArr[i] - exonStartsArr[i]; if (exonStarts != "") { exonStarts = exonStarts ";" } exonStarts = exonStarts exonStartsArr[i]; if (exonEnds != "") { exonEnds = exonEnds ";" } exonEnds = exonEnds exonEndsArr[i]; if (exonLengths != "") { exonLengths = exonLengths ";" } exonLengths = exonLengths lengthDiff; } $14=$14; $15=exonStarts; $16=exonEnds; $17=exonLengths; if (strand == "-") { split(exonStarts, exonStartsArr, ";"); split(exonEnds, exonEndsArr, ";"); exonStarts=""; exonEnds=""; for (i=length(exonStartsArr); i>=1; i--) { if (exonStarts != "") { exonStarts = exonStarts ";" } exonStarts = exonStarts exonStartsArr[i]; if (exonEnds != "") { exonEnds = exonEnds ";" } exonEnds = exonEnds exonEndsArr[i]; } $15=exonStarts; $16=exonEnds; } print}' | awk 'BEGIN{FS=OFS=","} {sub(/;$/,"",$15); sub(/;$/,"",$16); print $0}' | awk -F, -v OFS=, '{
-    if ($4 == "-") {
-        split($15, starts, ";");
-        split($16, ends, ";");
-        split($17, lengths, ";");
-        rev_starts="";
-        rev_ends="";
-        rev_lengths="";
-        for (i = length(starts); i >= 1; i--) {
-            if (rev_starts != "") {
-                rev_starts = rev_starts ";";
-            }
-            if (rev_ends != "") {
-                rev_ends = rev_ends ";";
-            }
-            if (rev_lengths != "") {
-                rev_lengths = rev_lengths ";";
-            }
-            rev_starts = rev_starts starts[i];
-            rev_ends = rev_ends ends[i];
-            rev_lengths = rev_lengths lengths[i];
-        }
-        $15=rev_starts;
-        $16=rev_ends;
-        $17=rev_lengths;
-    }
-    print
-}' | awk -F, -v OFS=, '{
-    if ($17 ~ /^0;/) {
-        sub(/^0;/, "", $17);
-    }
-    if ($17 ~ /;0$/) {
-        sub(/;0$/, "", $17);
-    }
-    print
-}' >> "$output_file"
+  mysql -h "${host}" -u "${user}" -D "${database}" -e "SELECT bin,name,chrom,strand,txStart,txEnd,cdsStart,cdsEnd,exonCount,score,name2,cdsStartStat,cdsEndStat, GROUP_CONCAT(DISTINCT exonFrames SEPARATOR ';'), GROUP_CONCAT(DISTINCT exonStarts SEPARATOR ';'), GROUP_CONCAT(DISTINCT exonEnds SEPARATOR ';') FROM refGene WHERE name='${nm_number}' GROUP BY name" -N | awk -F'\t' 'BEGIN{OFS=","} {gsub(/,/,";", $14); gsub(/,/,";", $15); gsub(/,/,";", $16); split($14, exonFramesArr, ";"); split($15, exonStartsArr, ";"); split($16, exonEndsArr, ";"); exonLengths=""; for (i=1; i<=length(exonFramesArr); i++) { lengthDiff = exonEndsArr[i] - exonStartsArr[i]; exonStarts = exonStarts exonStartsArr[i] ";"; exonEnds = exonEnds exonEndsArr[i] ";"; exonLengths = exonLengths lengthDiff ";"; } $14=$14; $15=exonStarts; $16=exonEnds; $17=exonLengths; print}' >> "$output_file"
 done < "$input_file"
 
 # Print a message indicating that the columns have been updated
